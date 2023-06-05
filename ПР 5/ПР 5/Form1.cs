5cf2b67fa1fd38906729e5f5c76b1b8f18c1c041
novahadgi <135410622+novahadgi@users.noreply.github.com>
2023-06-05 08:35:09 +0300

    Add files via upload

---
+++ ПР 5/ПР 5/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ПР_5
+{
+    public partial class Form1 : Form
+    {
+        public static Bitmap BM = new Bitmap(Screen.PrimaryScreen.Bounds.Width, 
+            Screen.PrimaryScreen.Bounds.Height);
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition= FormStartPosition.CenterScreen;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics GH = Graphics.FromImage(BM as Image);
+            GH.CopyFromScreen(0, 0, 0, 0, BM.Size);
+
+            ScreenShot SI = new ScreenShot();
+
+            SI.ShowDialog();
+
+        }
+    }
+}
---
+++ ПР 5/ПР 5/ScreenShot.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ПР_5
+{
+    public partial class ScreenShot : Form
+    {
+        public ScreenShot()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Image = Form1.BM;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SFD = new SaveFileDialog();
+            SFD.Filter = "PNG|*.png|JPEG|*.jpg|GIF|*.gif|BMP|*.bmp";
+            if (SFD.ShowDialog() == DialogResult.OK)
+            {
+                Form1.BM.Save(SFD.FileName);
+            }
+
+        }
+    }
+}
